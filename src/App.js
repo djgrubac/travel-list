26441e660ecefd3f0818c00f93e44e9e0684db7d
djgrubac <grubacdjordje@gmail.com>
2023-08-25 17:03:47 +0200

    Packing list app

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   )
 }
@@ -99,10 +99,20 @@ const Item = ({ item, onDeleteItem, onToggleItem }) => {
   )
 }
 
-const Stats = () => {
+const Stats = ({ items }) => {
+  if (!items.length) return <p className='stats'><em>Start adding items on your packing list 🚀 </em></p>
+
+  const numItems = items.length;
+  const numPacked = items.filter(item => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
-    <footer>
-      <em>💼 You have X item on your list, and you already packed X (X%).</em>
+    <footer className='stats'>
+      {percentage === 100 ? 'You got everything! Ready to go ✈️' :
+        <em>
+          {`💼 You have ${numItems} item on your list, and you already packed ${numPacked} (${percentage}%).`}
+        </em>
+      }
     </footer>
   )
 }
